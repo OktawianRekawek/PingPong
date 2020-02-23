821482a207f734f130398c4be9acc42c81c7ad0c
Oktawian Rekawek <rekawek.oktawian@gmail.com>
2020-02-23 15:52:20 +0100

    Add ball

--- Unit1.cpp
+++ Unit1.cpp
@@ -8,10 +8,20 @@
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TForm1 *Form1;
+
+int dX = 5;
+int dY = 5;
+
 //---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner)
         : TForm(Owner)
 {
 }
 //---------------------------------------------------------------------------
- 
+
+void __fastcall TForm1::BallTimerTimer(TObject *Sender)
+{
+        Ball->Left += dX;
+        Ball->Top += dY;
+}
+//---------------------------------------------------------------------------
