a297bb7e7518b9875b2fe8bb3474824d46255b31
Oktawian Rekawek <rekawek.oktawian@gmail.com>
2020-02-27 18:51:37 +0100

    Export .exe file

--- Project1.cpp
+++ Project1.cpp
@@ -10,12 +10,12 @@ WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
         try
         {
                  Application->Initialize();
-                 AnsiString strMessage = "Witaj w grze PingPong.\n\n"+
-                        "Lewy gracz steruje wciskaj¹c klawisze A oraz Z.\n"+
-                        "Prawy gracz steruje wciskaj¹c strza³ki do góry i w dó³.\n"+
-                        "\nDla urozmaicenia zabawy:\n"+
-                        "Odbicie pi³eczki zmienia k¹t w zale¿noœci od miejsca odbicia\n"+
-                        "Co 5 odbiæ pi³eczka przyspiesza, a¿ do osi¹gniêcia maksymalnej prêdkoœci\n\n"+
+                 AnsiString strMessage = "Witaj w grze PingPong.\n\n"
+                        "Lewy gracz steruje wciskaj¹c klawisze A oraz Z.\n"
+                        "Prawy gracz steruje wciskaj¹c strza³ki do góry i w dó³.\n"
+                        "\nDla urozmaicenia zabawy:\n"
+                        "Odbicie pi³eczki zmienia k¹t w zale¿noœci od miejsca odbicia\n"
+                        "Co 5 odbiæ pi³eczka przyspiesza, a¿ do osi¹gniêcia maksymalnej prêdkoœci\n\n"
                         "Mi³ej zabawy!";
                  ShowMessage(strMessage);
                  Application->CreateForm(__classid(TForm1), &Form1);
